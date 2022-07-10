377f93cf44ead764bb2f133aaba91c560d78d22d
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-07-09 20:29:51 -0400

    add login/logout functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -37,4 +37,16 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
 module.exports = router;
