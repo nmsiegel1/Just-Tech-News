71156074fc9e18b51f71991469403dfdf27ed786
Neenyo <102773691+nmsiegel1@users.noreply.github.com>
2022-07-10 14:53:40 -0400

    introduce helper functions to refactor the code and write tests

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,7 +23,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
